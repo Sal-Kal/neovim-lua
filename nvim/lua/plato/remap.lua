a6e744046dfb55f66c03cec3d80a27aee2102161
Sal-Kal <saishkalbag@gmail.com>
2023-01-11 11:52:14 +0530

    really really minor change

--- nvim/lua/plato/remap.lua
+++ nvim/lua/plato/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "H", "0")
+vim.keymap.set("n", "H", "_")
 vim.keymap.set("n", "L", "$")
 
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
